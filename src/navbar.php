0207039c38d80de330a858a9f2cdd5cfbf3fb549
auffray <auffray@et.esiea.fr>
2025-02-26 13:25:24 +0100

    feat: :art: Creation d'un fichier special navbar

---
+++ src/navbar.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Calendrier</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Rendez-vous</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="reserver.php">Prendre RDV</a></li>
+                    <li class="nav-item"><a class="nav-link" href="mes_rdv.php">Mes RDV</a></li>
+                    
+                    <?php if (isset($_SESSION['id'])): ?>
+                        <li class="nav-item"><a class="nav-link" href="profil.php">Mon Profil</a></li>
+                        <li class="nav-item"><a class="nav-link btn btn-danger text-white" href="disconect.php">Déconnexion</a></li>
+                    <?php else: ?>
+                        <li class="nav-item"><a class="nav-link btn btn-success text-white" href="login.php">Connexion</a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
